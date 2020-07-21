43cb91c1e1c41d86d8a27ad7996d9dfefe1add09
Stuart Ryan <45078089+Sentinion@users.noreply.github.com>
2020-07-21 12:46:34 -0700

    Coding done to home screen
    Made a class to hold the tickers on the main screen

--- PythiaStoryboard/PythiaStoryboard/HomeScreenViewController.swift
+++ PythiaStoryboard/PythiaStoryboard/HomeScreenViewController.swift
@@ -11,16 +11,59 @@ class HomeScreenViewController: UIViewController {
 
 
     @IBOutlet weak var portfolioLabel: UILabel!
-    
+    @IBOutlet weak var buyingPowerLabel: UILabel!
     @IBOutlet weak var portfolioPerformance: UILabel!
+    
+    // tickShareList is a list of all the tickers a user has investments in
+    var tickShareList = [TickerShareCombo]()
+    var buyingPower = 0
+    var portfolioValue : Double = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        let portfolioValue = 10235.25
+        portfolioValue = 10235.25
         let portfolioValueString: String = String(format: "%.2f", portfolioValue)
-        portfolioLabel.text = "Portfolio Value: "
+        portfolioLabel.text = "Portfolio Value: $"
             + portfolioValueString
+        buyingPower = 1500
+        let buyingPowerString: String = String(format: "%.2f", buyingPower)
+        buyingPowerLabel.text = "Buying Power: $" + buyingPowerString
+    }
+    //Should be run every second or two seconds to make sure the user's portfolio value is constantly kept up to date
+    func computePortfolioValue(){
+        tickShareList.forEach { tickShare in
+            tickShare.updatePrice()
+            if (tickShare.difference != 0){
+                portfolioValue += tickShare.difference
+                tickShare.difference = 0
+            }
+        }
+    }
+}
+
+class TickerShareCombo {
+    var ticker : String
+    var numSharesOwned : Int
+    var currentSharePrice : Double
+    var historicalSharePrice : Double
+    var difference : Double = 0
+    
+    init(ticker : String, numSharesOwned : Int, currentSharePrice : Double){
+        self.ticker = ticker
+        self.numSharesOwned = numSharesOwned
+        self.currentSharePrice = currentSharePrice
+        self.historicalSharePrice = currentSharePrice
+    }
+    
+    // checkPrice
+    func updatePrice(){
+        //TODO set currentSharePrice here to be equal the tickers current share price
+        if (historicalSharePrice != currentSharePrice) {
+            difference = currentSharePrice - historicalSharePrice
+            historicalSharePrice = currentSharePrice
+        }
     }
 }
 
