08b5f08451561447804acf43cc3fd906b5868a64
Nathaniel <veimaun@gmail.com>
2020-07-28 21:20:07 -0700

    adding new view controller

---
+++ PythiaStoryboard/PythiaStoryboard/ShareInfoViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ShareInfoViewController.swift
+//  PythiaStoryboard
+//
+//  Created by Nathaniel Veimau (old comp) on 7/28/20.
+//  Copyright © 2020 Pythia. All rights reserved.
+//
+
+import UIKit
+
+class ShareInfoViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+}
