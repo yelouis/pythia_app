fd9d40e91d4c5be110bea0b5f4c1610e9f727b22
Stuart Ryan <45078089+Sentinion@users.noreply.github.com>
2020-08-02 15:51:00 -0700

    Made buy and sell conditions

--- PythiaStoryboard/PythiaStoryboard/AlgorithmInfoViewController.swift
+++ PythiaStoryboard/PythiaStoryboard/AlgorithmInfoViewController.swift
@@ -16,31 +16,68 @@ class AlgorithmInfoViewController: UIViewController {
     
     @IBOutlet weak var showStockTrends: UIButton!
     @IBOutlet weak var tickerForAlgorithm: UILabel!
-    //Buy conditions outlets
-    @IBOutlet weak var buyStackView: UIStackView!
+    //Conditions outlets
     @IBOutlet weak var buyDropButton: UIButton!
     @IBOutlet weak var buyConditions: UITableView!
+        
+    @IBOutlet weak var sellDropButton: UIButton!
+    @IBOutlet weak var sellConditions: UITableView!
     
     //Algo information outlets
     @IBOutlet weak var algorithmName: UILabel!
     @IBOutlet weak var dynamicAlgorithmPrice: UILabel!
     @IBOutlet weak var currentShareValueLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
-           super.viewDidLoad()
+        super.viewDidLoad()
+        
+        buyDropButton.imageView?.contentMode = .scaleAspectFit
+        buyDropButton.imageView?.frame = CGRect(
+            origin: buyDropButton.imageView!.center,
+            size: CGSize(
+                width: buyDropButton.frame.width / 2,
+                height: buyDropButton.frame.height / 2))
+        
         //transfer in from previous view controller
         algorithmName.text = "myAlgo"
         tickerForAlgorithm.text = "WORK"
         dynamicAlgorithmPrice.text = "$1483.45"
         showStockTrends.titleLabel?.numberOfLines=3
         
+        //ConditionTableView shit
+        buyConditions.isHidden = true
     }
     
     @IBAction func showBuyConditions(_ sender: Any) {
+        if buyConditions.isHidden {
+            animate(toggle: true, conditions: buyConditions, buttonPressed: buyDropButton)
+        } else {
+            animate(toggle: false, conditions: buyConditions, buttonPressed: buyDropButton)
+        }
+    }
+    
+    func animate(toggle: Bool, conditions : UITableView, buttonPressed : UIButton) {//Need to have button to change and table to show as arguments in this function
+        if toggle {
+            //Will need to have arrow change to be upwards facing
+            UIView.animate(withDuration: 0.3) {
+                conditions.isHidden = false
+                buttonPressed.imageView?.image?.rotate(radians: .pi)
+            }
+        } else {
+            //Will need to have arrow change to be downwards facing
+            UIView.animate(withDuration: 0.3) {
+                conditions.isHidden = true
+                buttonPressed.imageView?.image?.rotate(radians: .pi)
+            }
+        }
     }
 }
 
 extension AlgorithmInfoViewController : UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //This will become a segue
         print("you tapped me and I am a condition")
     }
     
@@ -49,11 +86,39 @@ extension AlgorithmInfoViewController : UITableViewDelegate, UITableViewDataSour
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "investment", for: indexPath)as! StockTableViewCell
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: "condition",
+            for: indexPath)//as! ConditionViewCell
         cell.textLabel?.text = conditionList[indexPath.row]
         return cell
     }
     
+    
+}
+
+extension UIImage {
+    func rotate(radians: CGFloat) -> UIImage {
+        let rotatedSize = CGRect(origin: .zero, size: size)
+            .applying(CGAffineTransform(rotationAngle: CGFloat(radians)))
+            .integral.size
+        UIGraphicsBeginImageContext(rotatedSize)
+        if let context = UIGraphicsGetCurrentContext() {
+            let origin = CGPoint(x: rotatedSize.width / 2.0,
+                                 y: rotatedSize.height / 2.0)
+            context.translateBy(x: origin.x, y: origin.y)
+            context.rotate(by: radians)
+            draw(in: CGRect(x: -origin.y, y: -origin.x,
+                            width: size.width, height: size.height))
+            let rotatedImage = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+
+            return rotatedImage ?? self
+        }
+
+        return self
+    }
+}
+class ConditionViewCell : UITableViewCell {
     
 }
 
