257b01e80c430060b7e80bce499a051d7a7cabcd
Stuart Ryan <45078089+Sentinion@users.noreply.github.com>
2020-08-02 17:40:34 -0700

    Making template for adding new conditions

---
+++ PythiaStoryboard/PythiaStoryboard/ConditionView.swift
@@ -0,0 +1,21 @@
+//
+//  ConditionView.swift
+//  PythiaStoryboard
+//
+//  Created by Stuart on 8/2/20.
+//  Copyright © 2020 Pythia. All rights reserved.
+//
+
+import UIKit
+
+class ConditionView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
