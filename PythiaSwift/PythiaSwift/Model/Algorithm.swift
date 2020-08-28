8a55479503ced23d04594abe31c017b22c1dcca8
Louis Ye <yel@carleton.edu>
2020-08-27 19:59:28 -0700

    working on reading JSON

---
+++ PythiaSwift/PythiaSwift/Model/Algorithm.swift
@@ -0,0 +1,34 @@
+/*
+    Algorithm.swift
+    PythiaSwift
+ 
+    Created by Louis Ye on 8/27/20.
+    Copyright © 2020 Louis Ye. All rights reserved.
+ 
+    Abstract:
+    The model for an individual landmark.
+*/
+
+import SwiftUI
+import CoreLocation
+
+struct Algorithm: Hashable, Codable, Identifiable {
+    var id: Int
+    var name: String
+    var buyCondition: BuyCondition
+    var sellCondition: SellCondition
+    var algoReturns : AlgoReturns
+    fileprivate var smallGraph: String
+    fileprivate var bigGraph: String
+
+}
+
+extension Algorithm {
+    var smallImage: Image {
+        ImageStore.shared.image(name: smallGraph)
+    }
+    
+    var bigImage: Image {
+        ImageStore.shared.image(name: bigGraph)
+    }
+}
