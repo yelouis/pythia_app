d5fc24ec0ae40c34a1ac36cbaf4e2c5f5430122f
Louis Ye <yel@carleton.edu>
2020-09-02 14:33:04 -0700

    working on implementing environmental objects

---
+++ PythiaSwift/PythiaSwift/UserData.swift
@@ -0,0 +1,15 @@
+//
+//  UserData.swift
+//  PythiaSwift
+//
+//  Created by Louis Ye on 9/2/20.
+//  Copyright © 2020 Louis Ye. All rights reserved.
+//
+
+import SwiftUI
+import Combine
+
+final class UserData: ObservableObject{
+    @Published var algorithms = algorithmData
+}
+
